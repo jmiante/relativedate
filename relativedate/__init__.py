b65e469b262be68c96da0a5ea8a750d85e5c8c93
Jonathas Henrique <jonathasme@gmail.com>
2023-05-22 12:59:25 -0300

    Create Lib

--- relativedate/__init__.py
+++ relativedate/__init__.py
@@ -1,8 +1,40 @@
-from datetime import datetime
+from datetime import datetime as dt
 
 
 class RelativeDate:
-    def __init__(self):
-        pass
+    def __init__(self, datetime=None):
+        if datetime is None:
+            self.datetime = dt.now()
+        else:
+            self.datetime = datetime
+
+        self.year = self.datetime.year
+        self.month = self.datetime.month
+        self.day = self.datetime.day
+        self.hour = self.datetime.hour
+        self.minute = self.datetime.minute
+        self.second = self.datetime.second
+    
+    def __str__(self):
+        return self.datetime.strftime('%Y-%m-%d')
+
+    def get_relative_month(self, relative_month):
+        mes = self.month + relative_month
+        ano = self.year
+            
+        # Tratado Ano
+        if abs(relative_month) > 12:
+            ano_delta = int(relative_month / 12)
+            ano += ano_delta
+            mes += (abs(ano_delta) * 12) 
+        
+        # Tratamento do Mês
+        if mes < 1: 
+            mes += 12
+            ano -= 1
+        elif mes > 12: 
+            mes -= 12
+            ano += 1
+        return self.datetime.replace(year=ano, month=mes)
 
     
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='relativedate',
+    version='0.1.0',
+    description='Pacote Python para Datas Relativas, o projeto está iniciando, porém com o tempo traremos grandes melhorias',
+    packages=['relativedate'],
+    install_requires=[],
+)
