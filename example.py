3a0fde50a38d4750f5a35bf622dfdb3fa51f53c8
Jonathas Henrique <jonathasme@gmail.com>
2023-05-22 14:06:07 -0300

    README

--- example.py
+++ example.py
@@ -3,5 +3,7 @@ from relativedate import RelativeDate
 
 dt = datetime(year=2023, month=5, day=7)
 
+print(dt)
+
 rd = RelativeDate(dt)
-print(rd.get_relative_month(-9))
+print(rd.get_relative_month(-29))
